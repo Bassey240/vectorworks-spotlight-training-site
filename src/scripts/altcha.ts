b6e30dcbd2dff787d0725444f098c1d5a5e3c734
Sebastiaan ten Broek <sebastiaan.tenbroek@gmail.com>
2026-04-18 07:47:05 +0200

    Switch ALTCHA to official external CSP setup

--- src/scripts/altcha.ts
+++ src/scripts/altcha.ts
@@ -1,2 +1,27 @@
-import "altcha";
+import "altcha/external";
+import "altcha/altcha.css";
 import "altcha/i18n/nl";
+import pbkdf2WorkerUrl from "altcha/workers/pbkdf2?url";
+import shaWorkerUrl from "altcha/workers/sha?url";
+
+type AltchaGlobal = {
+  algorithms: Map<string, () => Worker>;
+};
+
+const altchaGlobal = (globalThis.$altcha ?? {}) as Partial<AltchaGlobal>;
+
+if (!altchaGlobal.algorithms) {
+  altchaGlobal.algorithms = new Map<string, () => Worker>();
+  globalThis.$altcha = altchaGlobal as typeof globalThis.$altcha;
+}
+
+const algorithms = altchaGlobal.algorithms;
+
+const createWorker = (url: string) => () => new Worker(url);
+
+algorithms.set("SHA-256", createWorker(shaWorkerUrl));
+algorithms.set("SHA-384", createWorker(shaWorkerUrl));
+algorithms.set("SHA-512", createWorker(shaWorkerUrl));
+algorithms.set("PBKDF2/SHA-256", createWorker(pbkdf2WorkerUrl));
+algorithms.set("PBKDF2/SHA-384", createWorker(pbkdf2WorkerUrl));
+algorithms.set("PBKDF2/SHA-512", createWorker(pbkdf2WorkerUrl));
